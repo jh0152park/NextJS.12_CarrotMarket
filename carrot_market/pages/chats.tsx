6b2691988f111df7e8197a004a9c4bf6733d38ea
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-07 23:56:13 +0900

    chats screen

---
+++ carrot_market/pages/chats.tsx
@@ -0,0 +1,20 @@
+export default function Chats() {
+    return (
+        <div className="py-10 divide-y-[1px]">
+            {[1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+                <div
+                    key={i}
+                    className="flex items-center px-4 py-3 space-x-3 cursor-pointer"
+                >
+                    <div className="w-10 h-10 rounded-full bg-slate-300" />
+                    <div>
+                        <p className="text-gray-700 ">Steve Jebs</p>
+                        <p className="text-sm text-gray-700">
+                            See you tomorrow in the corner at 2pm!
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}

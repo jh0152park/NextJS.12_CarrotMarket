3f710c9555f32de9683854236c10bd9111583c96
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-08 00:58:19 +0900

    detail chat

--- carrot_market/pages/chats/[id].tsx
+++ carrot_market/pages/chats/[id].tsx
@@ -2,24 +2,39 @@ import type { NextPage } from "next";
 
 const ChatDetail: NextPage = () => {
     return (
-        <div>
-            <div>
-                <div />
-                <div>Hi how much are you selling them for?</div>
-            </div>
-            <div>
-                <div />
-                <div>I want ￦20,000</div>
-            </div>
-            <div>
-                <div />
-                <div>미쳤어</div>
-            </div>
-            <div>
-                <div>
-                    <input type="text" />
-                    <div>
-                        <span>&rarr;</span>
+        <div className="px-4 py-10 space-y-4">
+            {[1, 2, 3, 4, 5, 6, 7].map((_, i) => (
+                <>
+                    <div key={i} className="flex items-start space-x-2">
+                        <div className="w-8 h-8 rounded-full bg-slate-400" />
+                        <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
+                            <p>Hi how much are you selling them for?</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+                        <div className="w-8 h-8 rounded-full bg-slate-400" />
+                        <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
+                            <p>I want ￦20,000</p>
+                        </div>
+                    </div>
+                    <div className="flex items-start space-x-2">
+                        <div className="w-8 h-8 rounded-full bg-slate-400" />
+                        <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
+                            <p>미쳤어</p>
+                        </div>
+                    </div>
+                </>
+            ))}
+            <div className="fixed inset-x-0 left-0 right-0 w-full max-w-md mx-auto bottom-2">
+                <div className="relative flex items-center">
+                    <input
+                        type="text"
+                        className="w-full pr-12 border-gray-300 rounded-full shadow-sm focus:ring-orange-500 focus:out-line-none focus:border-orange-500"
+                    />
+                    <div className="absolute inset-y-0 right-0 flex py-2 pr-2">
+                        <button className="flex items-center px-3 text-sm text-white bg-orange-500 rounded-full focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 hover:bg-orange-600">
+                            &rarr;
+                        </button>
                     </div>
                 </div>
             </div>

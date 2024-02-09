b44caa38b922438c2c1674fff894f8c02b5bff34
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-09 22:44:05 +0900

    streams create

--- carrot_market/pages/streams/[id].tsx
+++ carrot_market/pages/streams/[id].tsx
@@ -4,10 +4,25 @@ export default function LiveDetail() {
             <div className="pt-4">
                 <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
                 <h3 className="font-bold text-2xl mt-2 text-gray-900 ">
-                    Let's try potatos!
+                    Galaxy S50
                 </h3>
+                <h4 className="font-medium text-xl text-gray-700">$140</h4>
+                <p className="my-6 text-base text-gary-700">
+                    My money&apos;s in that office, right? If she start giving
+                    me some bullshit about it ain&apos;t there, and we got to go
+                    someplace else and get it, I&apos;m gonna shoot you in the
+                    head then and there. Then I&apos;m gonna shoot that bitch in
+                    the kneecaps, find out where my goddamn money is. She gonna
+                    tell me too. Hey, look at me when I&apos;m talking to you,
+                    motherfucker. You listen: we go in there, and that ni**a
+                    Winston or anybody else is in there, you the first
+                    motherfucker to get shot. You understand?
+                </p>
             </div>
 
+            <h4 className="font-bold text-2xl mt-2 text-gray-900 ">
+                Live Chat
+            </h4>
             <div className="py-10 pb-10 h-[50vh] overflow-y-scroll space-y-4">
                 {[...new Array(50)].map((_, i) => (
                     <>

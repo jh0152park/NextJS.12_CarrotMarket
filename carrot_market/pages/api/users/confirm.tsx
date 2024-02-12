12fdb00b84599891726c8a688148dbf746593926
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-12 22:55:29 +0900

    serverless session

--- carrot_market/pages/api/users/confirm.tsx
+++ carrot_market/pages/api/users/confirm.tsx
@@ -1,3 +1,5 @@
+import { cookies } from "next/headers";
+import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@/libs/server/client";
 import withHandler, { IResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -7,9 +9,27 @@ async function handler(
     res: NextApiResponse<IResponseType>
 ) {
     const { token } = req.body;
+    const isExistToken = await client.token.findUnique({
+        where: {
+            playload: token,
+        },
+    });
+
+    if (!isExistToken) {
+        res.status(404).end();
+    }
 
     console.log(`received token at api/users/confirm side is: ${token}`);
+    req.session.user = {
+        id: isExistToken?.userId,
+    };
+    // user에게 쿠키 주기
+    await req.session.save();
     res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+    cookieName: "carrotsession",
+    password:
+        "carrotmarkettestpassword1!carrotmarkettestpassword1!carrotmarkettestpassword1!carrotmarkettestpassword1!",
+});

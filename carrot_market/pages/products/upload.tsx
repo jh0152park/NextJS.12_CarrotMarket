b6c0a09a1c12650d8d942da09e5e4686689dffc5
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-15 15:24:59 +0900

    added upload prouduct form

--- carrot_market/pages/products/upload.tsx
+++ carrot_market/pages/products/upload.tsx
@@ -2,12 +2,31 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import Layout from "@/components/layout";
 import Textarea from "@/components/textarea";
+import useMutation from "@/libs/client/useMutation";
 import type { NextPage } from "next";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface IUploadProductsForm {
+    name: string;
+    price: number;
+    description: string;
+}
 
 const Upload: NextPage = () => {
+    const { register, handleSubmit, reset } = useForm<IUploadProductsForm>();
+    const [uploadProduct, { data, loading }] = useMutation("/api/products");
+
+    function onSubmit(data: FieldValues | IUploadProductsForm) {
+        if (loading) return;
+        uploadProduct(data);
+    }
+
     return (
         <Layout canGoBack>
-            <div className="px-4 py-16 space-y-5">
+            <form
+                className="px-4 py-16 space-y-5"
+                onSubmit={handleSubmit(onSubmit)}
+            >
                 <div>
                     <label className="flex items-center justify-center w-full h-48 text-gray-600 border-2 border-gray-300 border-dashed rounded-md hover:text-orange-500 hover:border-orange-500">
                         <svg
@@ -29,19 +48,26 @@ const Upload: NextPage = () => {
                     </label>
                 </div>
 
-                <Input name="name" label="Name" kind="text" />
-                <Input name="price" label="Price" kind="price" />
+                <Input
+                    register={register("name", { required: true })}
+                    name="name"
+                    label="Name"
+                    kind="text"
+                />
+                <Input
+                    register={register("price", { required: true })}
+                    name="price"
+                    label="Price"
+                    kind="price"
+                />
+                <Textarea
+                    register={register("description", { required: true })}
+                    placeholder=""
+                    label="Description"
+                />
 
-                <div>
-                    <label className="block mb-1 text-sm font-medium text-gary-700">
-                        Description
-                    </label>
-
-                    <Textarea placeholder="" />
-                </div>
-
-                <Button name="Upload product" />
-            </div>
+                <Button name={loading ? "Loading..." : "Upload product"} />
+            </form>
         </Layout>
     );
 };

cc1a039ed7a394398f6a8b7364238db457388177
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-02-28 00:10:45 +0900

    added bought sold favorite product component

---
+++ carrot_market/components/productList.tsx
@@ -0,0 +1,39 @@
+import { IProduct } from "@/pages";
+import useSWR from "swr";
+import ProductSummary from "./productSummary";
+
+interface IProductListProps {
+    kind: "favorite" | "sales" | "purchases";
+}
+
+interface IRecord {
+    id: number;
+    product: IProduct;
+}
+
+interface IProductListResponse {
+    [key: string]: IRecord[];
+}
+
+export default function ProductList({ kind }: IProductListProps) {
+    const { data } = useSWR<IProductListResponse>(`/api/users/me/${kind}`);
+    if (!data) {
+        return null;
+    }
+
+    return (
+        <>
+            {data[kind]?.map((record) => (
+                <ProductSummary
+                    product={record.product.name}
+                    description={record.product.description}
+                    price={record.product.price}
+                    like={record.product._count.favorite}
+                    // message={123}
+                    id={record.product.id}
+                    key={record.id}
+                />
+            ))}
+        </>
+    );
+}
--- carrot_market/pages/profile/loved.tsx
+++ carrot_market/pages/profile/loved.tsx
@@ -1,21 +1,11 @@
 import Layout from "@/components/layout";
-import ProductSummary from "@/components/productSummary";
+import ProductList from "@/components/productList";
 
 export default function Love() {
     return (
-        <Layout canGoBack>
+        <Layout canGoBack title="관심목록">
             <div className="flex flex-col py-10 space-y-5">
-                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-                    <ProductSummary
-                        product="iPhone 20 Pro Max"
-                        description="White"
-                        price={200}
-                        like={99}
-                        message={123}
-                        id={i}
-                        key={i}
-                    />
-                ))}
+                <ProductList kind="favorite" />
             </div>
         </Layout>
     );

1ab8e38d33774287b5060b9bf4b1db5ead61cbb9
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-14 15:10:44 +0900

    middleware redirection

--- carrot_market/middleware.ts
+++ carrot_market/middleware.ts
@@ -1,7 +1,23 @@
-import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse } from "next/server";
+import { NextRequest, NextFetchEvent, userAgent } from "next/server";
 
 export function middleware(request: NextRequest, event: NextFetchEvent) {
     console.log("it works! global middleware");
+
+    if (userAgent(request).isBot) {
+        return new Response("Please don't be a bot. Be human.", {
+            status: 403,
+        });
+    }
+
+    if (
+        !request.url.includes("/api") &&
+        !request.url.includes("/enter") &&
+        !request.cookies.has("carrotsession")
+    ) {
+        return NextResponse.redirect("/enter");
+    }
+
     if (request.nextUrl.pathname.startsWith("/chats")) {
         console.log("chat only middleware");
     }

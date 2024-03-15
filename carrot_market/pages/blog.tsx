2d7b31481d7671f4d2224f1329e74ae76732413f
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-15 15:53:34 +0900

    getStaticProps to make static page

--- carrot_market/pages/blog.tsx
+++ carrot_market/pages/blog.tsx
@@ -1,9 +1,40 @@
+import matter from "gray-matter";
 import Layout from "@/components/layout";
+import { readFileSync, readdirSync } from "fs";
+import { NextPage } from "next";
 
-export default function Blog() {
+interface IPost {
+    title: string;
+    date: string;
+    category: string;
+}
+
+const Blog: NextPage<{ posts: IPost[] }> = ({ posts }) => {
     return (
         <Layout title="Blog">
             <h1 className=" font-bold text-lg">Latest Posts</h1>
+            {posts.map((post, index) => (
+                <div key={index} className=" mb-5">
+                    <span className=" text-lg text-red-500">{post.title}</span>
+                    <div>
+                        <span>
+                            {post.date} / {post.category}
+                        </span>
+                    </div>
+                </div>
+            ))}
         </Layout>
     );
+};
+
+export async function getStaticProps() {
+    const posts = readdirSync("./posts").map((file) => {
+        const content = readFileSync(`./posts/${file}`, "utf-8");
+        return matter(content).data;
+    });
+    return {
+        props: { posts },
+    };
 }
+
+export default Blog;
